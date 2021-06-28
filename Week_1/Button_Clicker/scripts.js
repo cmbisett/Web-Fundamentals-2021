3db1e6b574fb701731fd6b49fdce5a91762e5a10
Cameron Bisett <cmbisett@gmail.com>
2021-06-27 21:01:50 -0700

    W1D5 Final

---
+++ Week_1/Button_Clicker/scripts.js
@@ -0,0 +1,13 @@
+function loginBtn (element) {
+    element.innerText = "Logout";
+}
+
+function removeBtn (element) {
+    element.remove();
+}
+
+function countLikes (element) {
+    var count = 13;
+    onclick = count++; 
+    element.innerText = count + ' Likes';
+}
