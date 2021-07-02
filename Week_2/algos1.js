cc20bfa3a2c8e2efd95b55f60bfaebe5b85b3854
Cameron Bisett <cmbisett@gmail.com>
2021-07-01 19:37:37 -0700

    algos_update

--- Week_2/algos1.js
+++ Week_2/algos1.js
@@ -40,63 +40,80 @@ console.log(prediction)
 // --------------------------------------------------------------------------------
 // ARRAYS OF OBJECTS PRACTICE
 
-var pokémon = [
-    { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
-    { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
-    { "id": 9,   "name": "Blastoise",  "types": ["water"] },
-    { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
-    { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
-    { "id": 23,  "name": "Ekans",      "types": ["poison"] },
-    { "id": 24,  "name": "Arbok",      "types": ["poison"] },
-    { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
-    { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
-    { "id": 52,  "name": "Meowth",     "types": ["normal"] },
-    { "id": 63,  "name": "Abra",       "types": ["psychic"] },
-    { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
-    { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
-    { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
-    { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
-    { "id": 98,  "name": "Krabby",     "types": ["water"] },
-    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
-    { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
-    { "id": 133, "name": "Eevee",      "types": ["normal"] },
-    { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
-    { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
-    { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
-    { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
-];
+// var pokémon = [
+//     { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
+//     { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
+//     { "id": 9,   "name": "Blastoise",  "types": ["water"] },
+//     { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
+//     { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
+//     { "id": 23,  "name": "Ekans",      "types": ["poison"] },
+//     { "id": 24,  "name": "Arbok",      "types": ["poison"] },
+//     { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
+//     { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
+//     { "id": 52,  "name": "Meowth",     "types": ["normal"] },
+//     { "id": 63,  "name": "Abra",       "types": ["psychic"] },
+//     { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
+//     { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
+//     { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
+//     { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
+//     { "id": 98,  "name": "Krabby",     "types": ["water"] },
+//     { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+//     { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
+//     { "id": 133, "name": "Eevee",      "types": ["normal"] },
+//     { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
+//     { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
+//     { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
+//     { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
+// ];
 
-// names of pokemon whos id's are greater than 99
-for(var i=0; i<pokémon.length; i++) {
-    if(pokémon[i].id > 99) {
-        console.log(pokémon[i].name);
+// // names of pokemon whos id's are greater than 99
+// for(var i=0; i<pokémon.length; i++) {
+//     if(pokémon[i].id > 99) {
+//         console.log(pokémon[i].name);
+//     }
+// }
+
+// //pokémon objects whose id is evenly divisible by 3
+// for(var i=0; i<pokémon.length; i++) {
+//     if(pokémon[i].id % 3 ==0) {
+//         console.log(pokémon[i])
+//     }
+// }
+
+// //pokemon thats have more than one type
+// for(var i=0; i<pokémon.length; i++) {
+//     if(pokémon[i].types.length > 1) {
+//         console.log(pokémon[i].name)
+//     }
+// }
+
+// //names of pokémon whose only type is poison
+// for(var i=0; i<pokémon.length; i++) {
+//     if(pokémon[i].types[0] == 'poison' && pokémon[i].types.length == 1) {
+//         console.log(pokémon[i].name)
+//     }
+// }
+
+// //console.log the first type of pokémon whose second type is 'flying'
+// for(var i=0; i<pokémon.length; i++) {
+//     if(pokémon[i].types[1] == 'flying') {
+//         console.log (pokémon[i].types[0])
+//     }
+// }
+
+//----------------------------------
+//FLATTEN ARRAYS/COMBINING ARRAYS INTO ONE
+//----------------------------------
+
+function flatten(arr2d) {
+    var flat = [];
+    for (var i=0; i<arr2d.length; i++) {
+        for (var t=0; t<arr2d[i].length; t++) {
+            flat.push(arr2d[i][t])
+        }
     }
+    return flat;
 }
 
-//pokémon objects whose id is evenly divisible by 3
-for(var i=0; i<pokémon.length; i++) {
-    if(pokémon[i].id % 3 ==0) {
-        console.log(pokémon[i])
-    }
-}
-
-//pokemon thats have more than one type
-for(var i=0; i<pokémon.length; i++) {
-    if(pokémon[i].types.length > 1) {
-        console.log(pokémon[i].name)
-    }
-}
-
-//names of pokémon whose only type is poison
-for(var i=0; i<pokémon.length; i++) {
-    if(pokémon[i].types[0] == 'poison' && pokémon[i].types.length == 1) {
-        console.log(pokémon[i].name)
-    }
-}
-
-//console.log the first type of pokémon whos second type is 'flying'
-for(var i=0; i<pokémon.length; i++) {
-    if(pokémon[i].types[1] == 'flying') {
-        console.log (pokémon[i].types[0])
-    }
-}
+var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
+console.log(result);
